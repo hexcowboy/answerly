928594cad942fa05f86a7f8cdb7c9bee97572c88
Jack Novotny <jacknovotny8@gmail.com>
2019-05-04 10:20:18 +0800

    add create answer functionality

--- django/qanda/views.py
+++ django/qanda/views.py
@@ -10,6 +10,38 @@ from .forms import (
 from .models import Question
 
 
+class AnswerCreateView(LoginRequiredMixin, CreateView):
+    form_class = AnswerForm
+    template_name = 'qanda/create_answer.html'
+
+    def get_initial(self):
+        return {
+            'question': self.get_question().id,
+            'user': self.request.user.id,
+        }
+
+    def get_context_data(self, **kwargs):
+        return super().get_context_data(
+            question=self.get_question(),
+            **kwargs
+        )
+
+    def get_success_url(self):
+        return self.object.question.get_absolute_url()
+
+    def form_valid(self, form):
+        action = self.request.POST.get('action')
+        if action == 'SAVE':
+            # save and redirect as usual
+            return super().form_valid(form)
+        elif action == 'PREVIEW':
+            context = self.get_context_data(preview=form.cleaned_data['answer'])
+            return HttpResponseBadRequest()
+
+    def get_question(self):
+        return Question.objects.get(pk=self.kwargs['pk'])
+
+
 class QuestionCreateView(LoginRequiredMixin, CreateView):
     form_class = QuestionForm
     template_name = 'qanda/ask.html'
