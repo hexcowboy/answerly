db2cf34ba0823766adb77ad75df07ce1f2366c60
Jack Novotny <jacknovotny8@gmail.com>
2019-06-10 18:19:23 -0700

    create QuestionFactory

---
+++ django/qanda/factories.py
@@ -0,0 +1,21 @@
+from unittest.mock import patch
+
+import factory
+
+from users.factories import UserFactory
+
+from .models import Question
+
+
+class QuestionFactory(factory.DjangoModelFactory):
+    title = factory.Sequence(lambda n: 'Question %d' % n)
+    question = 'What is a question?'
+    user = factory.SubFactory(UserFactory)
+
+    class Meta:
+        model = Question
+
+    @classmethod
+    def _create(cls, model_class, *args, **kwargs):
+        with patch('qanda.service.elasticsearch.ElasticSearch'):
+            return super()._create(model_class, *args, **kwargs)
