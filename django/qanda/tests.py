7111ea91501562f5d2b00dc5be73f533336a33d1
Jack Novotny <jacknovotny8@gmail.com>
2019-06-12 14:24:42 -0700

    create DailyQuestionListView tests

--- django/qanda/tests.py
+++ django/qanda/tests.py
@@ -1,12 +1,18 @@
+from datetime import date
+
 from unittest.mock import patch
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
 
 from elasticsearch import Elasticsearch
 
+from .factories import QuestionFactory
 from .models import Question
+from .views import DailyQuestionListView
+
+QUESTION_CREATED_STRFTIME = '%Y-%m-%d %H:%M'
 
 
 class QuestionSaveTestCase(TestCase):
@@ -46,3 +52,39 @@ class QuestionSaveTestCase(TestCase):
                 'doc_as_upsert': True,
             }
         )
+
+
+class DailyQuestionListTestCase(TestCase):
+    """
+    Tests the DailyQuestionListView
+    """
+    QUESTION_LIST_NEEDLE_TEMPLATE = '''
+    <li>
+        <a href="/q/{id}">{title}</a>
+        by {username} on {date}
+    </li>
+    '''
+    REQUEST = RequestFactory().get(path='/q/2030-12-31')
+    TODAY = date.today()
+
+    def test_GET_on_day_with_many_questions(self):
+        todays_questions = [QuestionFactory() for _ in range(10)]
+        response = DailyQuestionListView.as_view()(
+            self.REQUEST,
+            year=self.TODAY.year,
+            month=self.TODAY.month,
+            day=self.TODAY.day
+        )
+
+        self.assertEqual(200, response.STATUS_CODE)
+        self.assertEqual(10, response.context_data['object_list'].count())
+        rendered_content = response.rendered_content
+
+        for question in todays_questions:
+            needle = self.QUESTION_LIST_NEEDLE_TEMPLATE.format(
+                id=question.id,
+                title=question.title,
+                username=question.user.username,
+                date=question.created.strftime(QUESTION_CREATED_STRFTIME)
+            )
+            self.assertInHTML(needle, rendered_content)
