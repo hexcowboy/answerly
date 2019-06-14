ecb0365b0a50abb650575aa73af9921fff60b867
Jack Novotny <jacknovotny8@gmail.com>
2019-06-13 19:26:31 -0700

    configure for apache

--- django/config/wsgi.py
+++ django/config/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for config project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/
-"""
-
-import os
+import os, configparser
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+from django.core.wsgi import get_wsgi_application
+
+if not os.environ.get("DJANGO_SETTINGS_MODULE"):
+    parser = configparser.ConfigParser()
+    parser.read('/etc/answerly/answerly.ini')
+    for name, value in parser['mod_wsgi'].items():
+        os.environ[name.upper()] = value
 
 application = get_wsgi_application()
